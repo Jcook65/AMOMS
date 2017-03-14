248555652a25567578aea0abd56a70a063eb1835
Jonathan Cook <ohobs@PortableBox>
2017-03-14 18:33:05 -0400

    a bunch of stuff that im probably going to delete later. all of this for the login system

--- login/views.py
+++ login/views.py
@@ -1,9 +1,39 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
+
 
 def index(request):
-    template = loader.get_template('login/index.html')
-    context = {
+    context = {}
+    return render(request, 'login/index.html', context)
 
-    }
-    return HttpResponse(template.render(context, request))
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'login/register.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form':form})
+
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            # cleaned(normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns User objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('schedules:index')
+        return render(request, self.template_name, {'form':form})
--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'prescriptions'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 ]
