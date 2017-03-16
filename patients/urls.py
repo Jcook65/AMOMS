e6db723e057dcce56bebaee57d0eded0d8098f31
Ohobs <joncook2427@gmail.com>
2017-03-16 09:31:24 -0400

    working on patient app. NOT stable

--- patients/urls.py
+++ patients/urls.py
@@ -5,4 +5,5 @@ app_name = 'patients'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<patient_id>)$', views.DetailView.as_view(), name='detail'),
 ]
--- patients/views.py
+++ patients/views.py
@@ -4,7 +4,12 @@ from .models import Patient
 
 class IndexView(generic.ListView):
     template_name = 'patients/patients.html'
-    context_object_name = 'patient'
+    context_object_name = 'patients'
 
     def get_queryset(self):
         return Patient.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Patient
+    template_name = 'patient/detail.html'
