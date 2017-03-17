affb9cda3a8560551a2b98030f531a932f6acef2
Jonathan Cook <ohobs@PortableBox>
2017-03-16 20:08:53 -0400

    moved UserForm to inheret from User not Employee. Employee is unstable

--- login/forms.py
+++ login/forms.py
@@ -1,11 +1,12 @@
 from .models import Employee
+from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
-        model = Employee
+        model = User
         fields = ['username', 'email', 'password',]
 
 
